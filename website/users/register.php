dcb14f28f81e219b7ad1a21641873b2c0bfb0944
shamir-nci <x24244775@student.ncirl.ie>
2025-12-09 22:42:50 +0000

    update

--- website/users/register.php
+++ website/users/register.php
@@ -4,7 +4,7 @@ require_once("../include/users.php");
 require_once("../include/html_functions.php");
 require_once("../include/functions.php");
 
-session_start();
+// session_start();
 
 $error = False;
 if (isset($_POST['firstname']) && isset($_POST['username']) && isset($_POST['password']) && isset($_POST['againpass']) && isset($_POST['lastname'])
