5b910463115bdf218d5c29c6e8e39e334473548d
shamir-nci <x24244775@student.ncirl.ie>
2025-12-12 02:16:53 +0000

    update

---
+++ bootstrap.php
@@ -0,0 +1,4 @@
+<?php
+// Database compatibility layer
+require_once(__DIR__ . '/website/include/mysql-compat.php');
+?>
---
+++ test.php
@@ -0,0 +1,4 @@
+<?php
+require_once __DIR__ . '/mysql-compat.php';
+echo "Hello! PHP is working!";
+?>
